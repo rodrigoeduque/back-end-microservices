1701fddd1c2f73ddd88f08488d90cd2cc9c734be
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2022-02-12 10:58:23 -0300

    Model
    Adding UserModel

--- user-api/src/main/java/br/com/rodrigoeduque/app/userapi/model/User.java
+++ user-api/src/main/java/br/com/rodrigoeduque/app/userapi/model/User.java
@@ -1,2 +1,67 @@
-package br.com.rodrigoeduque.app.userapi.model;public class User {
+package br.com.rodrigoeduque.app.userapi.model;
+
+import br.com.rodrigoeduque.app.userapi.dto.UserDto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private LocalDate dataCadastro;
+
+    @Deprecated
+    public User() {
+    }
+
+    public User(String nome, String cpf, String endereco, String email, String telefone, LocalDate dataCadastro) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    private static User convertToModel(UserDto userDto) {
+        return new User(userDto.getNome(), userDto.getCpf(), userDto.getEndereco(), userDto.getEmail(), userDto.getTelefone(), userDto.getDataCadastro());
+    }
 }
