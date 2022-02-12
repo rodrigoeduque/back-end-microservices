8eb0582ba785680bbbf8df7401596e06fd0c7ab9
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2022-02-12 10:58:12 -0300

    DTO
    Adding UserDto UserResumidoDto

--- user-api/src/main/java/br/com/rodrigoeduque/app/userapi/dto/UserDto.java
+++ user-api/src/main/java/br/com/rodrigoeduque/app/userapi/dto/UserDto.java
@@ -1,2 +1,64 @@
-package br.com.rodrigoeduque.app.userapi.dto;public class UserDto {
+package br.com.rodrigoeduque.app.userapi.dto;
+
+import br.com.rodrigoeduque.app.userapi.model.User;
+
+import java.time.LocalDate;
+
+public class UserDto {
+
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private LocalDate dataCadastro;
+
+    public UserDto(String nome, String cpf, String endereco, String email, String telefone, LocalDate dataCadastro) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", dataCadastro=" + dataCadastro +
+                '}';
+    }
+
+    private static UserDto convertToDto(User user) {
+        return new UserDto(user.getNome(), user.getCpf(), user.getEndereco(), user.getEmail(), user.getTelefone(), user.getDataCadastro());
+    }
 }
--- user-api/src/main/java/br/com/rodrigoeduque/app/userapi/dto/UserResumidoDto.java
+++ user-api/src/main/java/br/com/rodrigoeduque/app/userapi/dto/UserResumidoDto.java
@@ -4,34 +4,20 @@ import br.com.rodrigoeduque.app.userapi.model.User;
 
 import java.time.LocalDate;
 
-public class UserDto {
+public class UserResumidoDto {
 
     private String nome;
-    private String cpf;
-    private String endereco;
     private String email;
     private String telefone;
-    private LocalDate dataCadastro;
 
-    public UserDto(String nome, String cpf, String endereco, String email, String telefone, LocalDate dataCadastro) {
+    public UserResumidoDto(String nome, String email, String telefone) {
         this.nome = nome;
-        this.cpf = cpf;
-        this.endereco = endereco;
         this.email = email;
         this.telefone = telefone;
-        this.dataCadastro = dataCadastro;
     }
 
     public String getNome() {
         return nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getEndereco() {
-        return endereco;
     }
 
     public String getEmail() {
@@ -42,23 +28,16 @@ public class UserDto {
         return telefone;
     }
 
-    public LocalDate getDataCadastro() {
-        return dataCadastro;
-    }
-
     @Override
     public String toString() {
         return "UserDto{" +
                 "nome='" + nome + '\'' +
-                ", cpf='" + cpf + '\'' +
-                ", endereco='" + endereco + '\'' +
                 ", email='" + email + '\'' +
                 ", telefone='" + telefone + '\'' +
-                ", dataCadastro=" + dataCadastro +
                 '}';
     }
 
-    private static UserDto convertToDto(User user) {
-        return new UserDto(user.getNome(), user.getCpf(), user.getEndereco(), user.getEmail(), user.getTelefone(), user.getDataCadastro());
+    private static UserResumidoDto convertToDto(User user) {
+        return new UserResumidoDto(user.getNome(), user.getEmail(), user.getTelefone());
     }
 }
