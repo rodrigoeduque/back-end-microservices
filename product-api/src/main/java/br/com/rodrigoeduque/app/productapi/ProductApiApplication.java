208fa434505bd5f3d6004cef5001b0fb05de4a12
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2022-02-19 10:54:44 -0300

    Foundation Module
    Product-api

---
+++ product-api/src/main/java/br/com/rodrigoeduque/app/productapi/ProductApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.rodrigoeduque.app.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProductApiApplication.class , args);
+	}
+
+}
---
+++ product-api/target/classes/db/migration/V1__create_category_table.sql
@@ -0,0 +1,4 @@
+create schema if not exists products;
+
+create table products.category (id bigserial primary key,
+nome varchar(100) not null);
